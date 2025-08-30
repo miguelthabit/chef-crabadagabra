95dfe8924b47c1ee2089df03f6b6c43ed2d6d7d3
miguelthabit <miguelndilahomba@gmail.com>
2025-08-30 19:36:50 +0300

    this is pattern

---
+++ chef crabadagabra.py
@@ -0,0 +1,9 @@
+print("We are going to print patterns")
+rows =7
+for i in range(1,rows+1):
+    print("#"*i)
+
+print("This is inverted pattern")
+rows =7
+for i in range(rows,0,-1):
+    print("#"*i)
